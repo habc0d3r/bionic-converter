1b203e14df5d1212e7a5f72612a967c4e398f6ea
habc0d3r <habcoder@gmail.com>
2023-03-15 23:34:08 +0530

    Change in main.py code

--- main.py
+++ main.py
@@ -3,26 +3,31 @@ import markdown
 
 def bionic():
     # opening and reading a text file
-    with open('sample_text.txt', 'r') as sample:
-        contents = sample.read()
+    try:
+        with open('sample_text.txt', 'r') as sample:
+            contents = sample.read()
+    except FileNotFoundError as error:
+        print(error)
 
     # split into list of words
     word_list = contents.split()
     # create new empty list
-    output_list = [] 
-    
+    output_list = []
+
     # convert the words and add to a list
     for i in range(len(word_list)):
         word = word_list.pop(0)
         # do the operation on the word
         if len(word) == 1:
             final_word = f'**{word}**'
-        elif len(word)%2 == 0:
-            final_word = '**{}{}**'.format(word[0], word[1:int(len(word)/2)]) + word[int(len(word)/2):]
-        elif len(word)%2 != 0:
-            final_word = '**{}{}**'.format(word[0], word[1:int(len(word)/2) + 1]) + word[int(len(word)/2)+1:]
+        elif len(word) % 2 == 0:
+            final_word = '**{}{}**'.format(
+                word[0], word[1:int(len(word)/2)]) + word[int(len(word)/2):]
+        elif len(word) % 2 != 0:
+            final_word = '**{}{}**'.format(
+                word[0], word[1:int(len(word)/2) + 1]) + word[int(len(word)/2)+1:]
         output_list.append(final_word)
-        
+
     # join the words of the list
     output = ' '.join(output_list)
 
@@ -35,7 +40,7 @@ def bionic():
     with open('bionic.md', 'r') as md_file:
         # Read the contents of the file
         md_contents = md_file.read()
-        
+
     # Convert the markdown to HTML
     html_contents = markdown.markdown(md_contents)
 
